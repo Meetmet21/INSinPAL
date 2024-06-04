28d3b09295193714b7e2cf5e2d79c7dad28c3d42
Meetmet21 <mehmetsehir1@gmail.com>
2024-06-04 16:57:49 +0200

    Update and reformat install.sh and rename INSinPAL conda environment file.

--- install.sh
+++ install.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-# Restrictive error handling
+################################################ GLOBAL VARIABLES ################################################
+
+
+# Error handling parameters
 set -o errexit
 set -o nounset
 set -o pipefail
 
-### Functions
+
+# Path to main directory
+MAIN_DIR=$(echo $PWD)
+
+
+################################################ FUNCTIONS ################################################
+
 
 # Print error message
 log_error() {
@@ -19,148 +28,124 @@ log_stdout() {
         echo -e 'Install log: '"$1"
 }
 
-### Global variables ###
-# Path to main directory
-MAIN_DIR=$(echo $PWD)
 
-### Conda ###
+################################################ CONDA ENVIRONMENT SETTINGS ################################################
+
 
 # Check for conda bin
-
 log_stdout "Checking for Conda bin."
 
 if ! $(command -v conda &> /dev/null)
 then
-
 	log_error "Conda is not detected and need to be installed."
-
 	exit 1
 else
-
 	log_stdout "Conda bin found."
-
 fi
 
+
 # INSinPAL conda environment setup
-
 log_stdout "Installing INSinPAL Conda environment."
-
 SNK_ENV_NAME="INSinPAL"
+
 # Snakemake environment already present
 if $(conda list --name "$SNK_ENV_NAME" &> /dev/null)
 then
-
         log_stdout ""$SNK_ENV_NAME" environment is already present."
 
 # Build INSinPAL conda environment then
-elif $(conda env create --file=resources/conda/snakemake_env.yaml)
+elif $(conda env create --file=resources/conda/environment.yml)
 then
-
         log_stdout ""$SNK_ENV_NAME" environment was successfully created."
-
 else
-
         log_error ""$SNK_ENV_NAME" environment coulnd't be build."
-
         exit 1
 fi
 
-# Activate Snakemake Conda env
 
+# Activate INSinPAL Conda env
 log_stdout "Activating INSinPAL Conda environment."
 
 if $(source activate "$SNK_ENV_NAME")
 then
-
         log_stdout ""$SNK_ENV_NAME" was successfully activated."
-
 else
-
         log_error ""$SNK_ENV_NAME" couldn't be activated."
-
         exit 1
 fi
 
-### Data ###
 
-log_stdout "Data acquisition: reference genome hg19 masked regions."
+################################################ DATA ACQUISITION ################################################
+
 
 # Reference genome hg19 masked repeat region from UCSC ftp
+log_stdout "Downloading reference genome hg19 with repeat masked regions."
+# Set up downlading DIR
 mkdir -p resources/data/Genome/hg19/chromosomes
 PATH_DATA_HG19="resources/data/Genome/hg19"
+# Final file name
 HG19_FASTA_NAME="genome_PAR_masked.fasta.gz"
 URL="https://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/hg19.fa.masked.gz"
+
 if [[ -f "$PATH_DATA_HG19"/"$HG19_FASTA_NAME" ]] || $(curl -o "$PATH_DATA_HG19"/"$HG19_FASTA_NAME" "$URL")
 then
-
         log_stdout "hg19 reference genome was successfully downloaded to "$PATH_DATA_HG19" from UCSC ftp."
-
 else
-
         log_error "hg19 reference genome couldn't be downloaded from UCSC ftp."
-
         exit 1
 fi
+
 
 # Uncompress reference genome OR skip if already exists
 if [[ -f "$PATH_DATA_HG19"/${HG19_FASTA_NAME%.gz} ]]
 then
-
         log_stdout "hg19 reference genome successfully uncompressed."
-
+	# Remove tar file
         rm "$PATH_DATA_HG19"/"$HG19_FASTA_NAME"
 elif $(gunzip -d "$PATH_DATA_HG19"/"$HG19_FASTA_NAME")
 then
-
         log_stdout "hg19 reference genome was successfully uncompressed."
-
 else
-
         log_error "hg19 reference genome couldn't be uncompressed by gunzip."
-
         exit 1
 fi
 
-log_stdout "Data acquisition: reference genome fasta per chromosome."
 
 # Reference genome hg19 masked repeat regions per chromosome
+log_stdout "Data acquisition: reference genome fasta per chromosome."
+# Rename tar to
 HG19_CHROMOSOMES="chromosomes.tar.gz"
 URL="https://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/chromFaMasked.tar.gz"
+
 if [[ -f "$PATH_DATA_HG19"/chromosomes/"$HG19_CHROMOSOMES" ]] || $(curl -o "$PATH_DATA_HG19"/chromosomes/"$HG19_CHROMOSOMES" "$URL")
 then
-
         log_stdout "hg19 reference genome chromosomes have been downloaded to "$PATH_DATA_HG19"/chromosomes from UCSC ftp."
-
 else
-
         log_error "hg19 reference genome chromosomes couldn't be downdloaded."
-
         exit 1
 fi
 
-# Untar chromosomes
+
+# Untar hg19 chromosomes
+
 if $(tar -xvzf "$PATH_DATA_HG19"/chromosomes/"$HG19_CHROMOSOMES" --directory "$PATH_DATA_HG19"/chromosomes &> /dev/null)
 then
-
         log_stdout "Chromosomes have been untared."
-
         # Remove tar file
         rm "$PATH_DATA_HG19"/chromosomes/"$HG19_CHROMOSOMES"
 else
-
         log_error "Untar process has failed with tar."
-
         exit 1
 fi
 
-# Rename chromosomes files to 1.fa than chr1.fa and remove files mathcing alternative alleles or MT
 
+# Rename chromosomes files to 1.fa instead of chr1.fa and remove files mathcing alternative alleles or MT
 log_stdout "Rename chromosomes files from chr1.fa.masked to 1.fa and remove unnecessary fasta files."
 
 for file in $(ls "$PATH_DATA_HG19"/chromosomes/*); do
-	# Filename
+	# Get filename
 	filename=$(basename "$file")
-	# Remove contif, alternative chr files and Mitochondrial file
+	# Remove contig, alternative chr files and Mitochondrial file
 	if [[ $(grep -cP "chr.+_" "$file") -eq 1 ]] || [[ $(grep -cP "chrM" "$file") -eq 1 ]];
 	then
 		rm "$file"
@@ -173,62 +158,53 @@ for file in $(ls "$PATH_DATA_HG19"/chromosomes/*); do
 	fi
 done
 
-### Progs ###
 
-log_stdout "Downloading needed software."
+################################################ SOFTWARE SETTINGS ################################################
 
-PROGS_DIR="resources/singularity"
+log_stdout "Downloading necessary software for INSinPAL environment."
+
 # Check for singularity
-
 log_stdout "Checking for singularity bin."
 
 if [[ $(command -v singularity /dev/null) ]]
 then
-
 	log_stdout "Singularity bin found."
-
 else
-
 	log_error "Singularity not found, please install it."
-
 	exit 1
 fi
 
+
 # INSurVeyor
+# DIrectory path for software
+PROGS_DIR="resources/singularity"
+log_stdout "Downloading INSurVeyor singularity image from https://github.com/kensung-lab/INSurVeyor/releases/download/1.1.2/insurveyor.sif"
+# DIrectory path for iNSurVeyor
 INSURVEYOR_DIR="$PROGS_DIR"/"INSurVeyor/1.1.2/"
 mkdir -p "$INSURVEYOR_DIR"
 URL="https://github.com/kensung-lab/INSurVeyor/releases/download/1.1.2/insurveyor.sif"
 
-log_stdout "Downloading INSurVeyor singularity image from https://github.com/kensung-lab/INSurVeyor/releases/download/1.1.2/insurveyor.sif"
-
 if $(wget --quiet --directory-prefix "$INSURVEYOR_DIR" "$URL")
 then
-
 	log_stdout "INSurVeyor was successfully donwloaded to "$INSURVEYOR_DIR"."
-
 else
-
 	log_error "INSurVeyor couldn't be downloaded."
-
 	exit 1
 fi
 
+
 # Manta
+log_stdout "Downloading Manta binary distribution from https://github.com/Illumina/manta/releases/download/v1.6.0/manta-1.6.0.centos6_x86_64.tar.bz2."
+# Directory path for software
 MANTA_DIR="$PROGS_DIR"/"manta/1.6.0/"
 mkdir -p "$MANTA_DIR"
 URL="https://github.com/Illumina/manta/releases/download/v1.6.0/manta-1.6.0.centos6_x86_64.tar.bz2"
 
-log_stdout "Downloading Manta binary distribution from https://github.com/Illumina/manta/releases/download/v1.6.0/manta-1.6.0.centos6_x86_64.tar.bz2."
-
 if $(wget --quiet --directory-prefix "$MANTA_DIR" "$URL")
 then
-
 	log_stdout "Manta 1.6.0 binary distribution was successfully downloaded to "$MANTA_DIR"."
-
 else
-
 	log_error "Manta couldn't be installed."
-
 	exit 1
 fi
 
@@ -239,49 +215,43 @@ if $(tar -xjvf "$MANTA_DIR"/$(basename "$URL") --directory "$MANTA_DIR" &> /dev/
 then
 	# Remove the compress file
 	rm "$MANTA_DIR"/$(basename "$URL")
-
 	log_stdout "Manta was successfully uncompressed in "$MANTA_DIR"."
-
 else
-
 	log_error "Manta couldn't be uncompressed."
-
 	exit 1
 fi
+
 
 # Basil
-
 log_stdout "Building Basil singularity image."
-
+# Directory path to software
 BASIL_DIR="$PROGS_DIR"/"Anis-Basil/1.2.0"
+
 if $(singularity build "$BASIL_DIR"/"anisebasil.sif" "$BASIL_DIR"/"Anise_Basil_1_2_0.def")
 then
-
 	log_stdout "Basil singularity image was successfully built in "$BASIL_DIR"."
-
 else
-
 	log_error "Basil singularity image couldn't be build."
-
 	exit 1
 fi
 
-# AnnotSV
 
-URL="https://github.com/lgmgeo/AnnotSV/archive/refs/tags/v3.4.2.tar.gz"
+# AnnotSV
 log_stdout "Downloading AnnotSV code source from "$URL"."
+# Directory path to software
 ANNOTSV_DIR="$PROGS_DIR"/"AnnotSV/3.4"
+URL="https://github.com/lgmgeo/AnnotSV/archive/refs/tags/v3.4.2.tar.gz"
+# Set directory tree
 mkdir -p "$ANNOTSV_DIR"
 
 if $(wget --quiet --directory-prefix "$ANNOTSV_DIR" "$URL")
 then
-
 	log_stdout "AnnotSV code source was succesfully downloaded to "$ANNOTSV_DIR"."
-
 else
 	log_error "AnnotSV couldn't be downloaded."
 	exit 1
 fi
+
 
 log_stdout "Untaring AnnotSV."
 
@@ -292,7 +262,9 @@ else
 	log_error "AnnotSV couldn't be untared."
 fi
 
+
 log_stdout "Building code source for AnnotSV."
+# Binary ditribution directory
 BUILD_DIR="$ANNOTSV_DIR"/"AnnotSV-3.4.2"
 
 if $(cd "$BUILD_DIR" && make PREFIX=. install && make PREFIX=. install-human-annotation)
@@ -303,4 +275,5 @@ else
 	exit 1
 fi
 
-### Test ###
+
+############################################### TEST ################################################
