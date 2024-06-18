c4022939fd40c6cc2c96f4a2b53bca6b02eb8a5d
mesehir <eseh@hcuge.ch>
2024-06-18 14:17:34 +0200

    Uptade install.sh: from command substitution to nothing to capture exit status. add bam.bai so no need of samtools.

--- install.sh
+++ install.sh
@@ -35,7 +35,7 @@ log_stdout() {
 # Check for conda bin
 log_stdout "Checking for Conda bin."
 
-if ! $(command -v conda &> /dev/null)
+if ! command -v conda &> /dev/null
 then
 	log_error "Conda is not detected and need to be installed."
 	exit 1
@@ -49,12 +49,12 @@ log_stdout "Installing INSinPAL Conda environment."
 SNK_ENV_NAME="INSinPAL"
 
 # Snakemake environment already present
-if $(conda list --name "$SNK_ENV_NAME" &> /dev/null)
+if conda list --name "$SNK_ENV_NAME" &> /dev/null
 then
         log_stdout ""$SNK_ENV_NAME" environment is already present."
 
 # Build INSinPAL conda environment then
-elif $(conda env create --quiet --yes --file resources/conda/environment.yml &> /dev/null)
+elif conda env create --yes --file resources/conda/environment.yml
 then
         log_stdout ""$SNK_ENV_NAME" environment was successfully created."
 else
@@ -66,7 +66,7 @@ fi
 # Activate INSinPAL Conda env
 log_stdout "Activating INSinPAL Conda environment."
 
-if $(source activate "$SNK_ENV_NAME")
+if source activate "$SNK_ENV_NAME"
 then
         log_stdout ""$SNK_ENV_NAME" was successfully activated."
 else
@@ -90,7 +90,7 @@ PATH_DATA_HG19="resources/data/Genome/hg19"
 HG19_FASTA_NAME="genome.masked.fasta"
 URL="https://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/initial/hg19.fa.masked.gz"
 
-if $(curl --output "$PATH_DATA_HG19"/"$HG19_FASTA_NAME".gz "$URL")
+if curl --output "$PATH_DATA_HG19"/"$HG19_FASTA_NAME".gz "$URL"
 then
         log_stdout "hg19 reference genome was successfully downloaded to "$PATH_DATA_HG19" from UCSC ftp."
 else
@@ -99,7 +99,7 @@ else
 fi
 
 # Uncompress reference genome OR skip if already exists
-if $(gunzip --uncompress "$PATH_DATA_HG19"/"$HG19_FASTA_NAME".gz)
+if gunzip --uncompress "$PATH_DATA_HG19"/"$HG19_FASTA_NAME".gz
 then
         log_stdout "hg19 reference genome was successfully uncompressed."
 else
@@ -109,7 +109,7 @@ fi
 
 log_stdout "Rename contigs from >chr1 to >1 in reference genome."
 
-if ! $(sed --in-place --regexp-extended "/^>/ s/chr//g" "${PATH_DATA_HG19}"/"${HG19_FASTA_NAME}")
+if ! sed --in-place --regexp-extended "/^>/ s/chr//g" "${PATH_DATA_HG19}"/"${HG19_FASTA_NAME}"
 then
 	log_error "Couldn't change the contig names with sed."
 	exit 1
@@ -117,7 +117,7 @@ fi
 
 log_stdout "Generating indexed hg19 fasta file."
 
-if $(samtools faidx "${PATH_DATA_HG19}"/"${HG19_FASTA_NAME}")
+if samtools faidx "${PATH_DATA_HG19}"/"${HG19_FASTA_NAME}"
 then
 	log_stdout "hg19 fasta file was successfully indexed."
 else
@@ -134,7 +134,7 @@ log_stdout "Data acquisition: reference genome fasta per chromosome."
 HG19_CHROMOSOMES="chromosomes.tar.gz"
 URL="https://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/initial/chromFa.tar.gz"
 
-if $(curl --output "$PATH_DATA_HG19"/chromosomes/"$HG19_CHROMOSOMES" "$URL")
+if curl --output "$PATH_DATA_HG19"/chromosomes/"$HG19_CHROMOSOMES" "$URL"
 then
         log_stdout "hg19 reference genome chromosomes have been downloaded to "$PATH_DATA_HG19"/chromosomes from UCSC ftp."
 else
@@ -145,7 +145,7 @@ fi
 
 # Untar hg19 chromosomes
 
-if $(tar -xvzf "$PATH_DATA_HG19"/chromosomes/"$HG19_CHROMOSOMES" --directory "$PATH_DATA_HG19"/chromosomes &> /dev/null)
+if tar -xvzf "$PATH_DATA_HG19"/chromosomes/"$HG19_CHROMOSOMES" --directory "$PATH_DATA_HG19"/chromosomes
 then
         log_stdout "Chromosomes have been untared."
         # Remove tar file
@@ -187,7 +187,7 @@ log_stdout "Downloading necessary software for INSinPAL environment."
 # Check for singularity
 log_stdout "Checking for singularity bin."
 
-if [[ $(command -v singularity /dev/null) ]]
+if command -v singularity > /dev/null
 then
 	log_stdout "Singularity bin found."
 else
@@ -207,7 +207,7 @@ INSURVEYOR_DIR="$PROGS_DIR"/"INSurVeyor/1.1.2/"
 mkdir -p "$INSURVEYOR_DIR"
 URL="https://github.com/kensung-lab/INSurVeyor/releases/download/1.1.2/insurveyor.sif"
 
-if $(wget --quiet --directory-prefix "$INSURVEYOR_DIR" "$URL")
+if wget --quiet --directory-prefix "$INSURVEYOR_DIR" "$URL"
 then
 	log_stdout "INSurVeyor was successfully donwloaded to "$INSURVEYOR_DIR"."
 else
@@ -225,7 +225,7 @@ MANTA_DIR="$PROGS_DIR"/"Manta/1.6.0/"
 mkdir -p "$MANTA_DIR"
 URL="https://github.com/Illumina/manta/releases/download/v1.6.0/manta-1.6.0.centos6_x86_64.tar.bz2"
 
-if $(wget --quiet --directory-prefix "$MANTA_DIR" "$URL")
+if wget --quiet --directory-prefix "$MANTA_DIR" "$URL"
 then
 	log_stdout "Manta 1.6.0 binary distribution was successfully downloaded to "$MANTA_DIR"."
 else
@@ -236,7 +236,7 @@ fi
 
 log_stdout "Untar Manta distribution file."
 
-if $(tar -xjvf "$MANTA_DIR"/$(basename "$URL") --directory "$MANTA_DIR" &> /dev/null)
+if tar -xjvf "$MANTA_DIR"/$(basename "$URL") --directory "$MANTA_DIR"
 then
 	# Remove the compress file
 	rm "$MANTA_DIR"/$(basename "$URL")
@@ -290,7 +290,7 @@ URL="https://github.com/lgmgeo/AnnotSV/archive/refs/tags/v3.4.2.tar.gz"
 # Set directory tree
 mkdir -p "$ANNOTSV_DIR"
 
-if $(wget --quiet --directory-prefix "$ANNOTSV_DIR" "$URL")
+if wget --quiet --directory-prefix "$ANNOTSV_DIR" "$URL"
 then
 	log_stdout "AnnotSV code source was succesfully downloaded to "$ANNOTSV_DIR"."
 else
@@ -301,7 +301,7 @@ fi
 
 log_stdout "Untaring AnnotSV."
 
-if (tar -xvzf "$ANNOTSV_DIR"/$(basename "$URL")  --directory "$ANNOTSV_DIR" &> /dev/null)
+if tar -xvzf "$ANNOTSV_DIR"/$(basename "$URL")  --directory "$ANNOTSV_DIR"
 then
 	log_stdout "AnnotSV was successfully untared."
 else
@@ -316,7 +316,12 @@ BUILD_DIR="$ANNOTSV_DIR"/"AnnotSV"
 mv "$ANNOTSV_DIR"/"AnnotSV-3.4.2" "$BUILD_DIR"
 
 # Build and capture exit code
-if $(cd "$BUILD_DIR" && make DESTDIR= PREFIX=. install &> /dev/null && make DESTDIR= PREFIX=. install-human-annotation &> /dev/null)
+(
+	cd "$BUILD_DIR" && \
+	make DESTDIR= PREFIX=. install && \
+	make DESTDIR= PREFIX=. install-human-annotation
+)
+if [[ "$?" -eq 0 ]]
 then
 	log_stdout "AnnotSV was successfully built to "$BUILD_DIR"."
 else
